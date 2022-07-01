f0e2e9c4e8b4425d24faefacf60b72dfac084db9
Luke Kreczko <lkreczko@googlemail.com>
2022-07-01 14:52:28 +0100

    added callback for log-level and implemented "get"

--- src/xrdsum/backends/__init__.py
+++ src/xrdsum/backends/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from ._base import XrdsumBackend
+from ._cephfs import CephFSBackend
 from ._hdfs import HDFSBackend
 
-FILE_SYSTEMS = {
+FILE_SYSTEMS: dict[str, type[XrdsumBackend]] = {
     "HDFS": HDFSBackend,
-    "CephFS": None,
+    "CephFS": CephFSBackend,
 }
 
 __all__ = ["FILE_SYSTEMS"]
--- src/xrdsum/cli.py
+++ src/xrdsum/cli.py
@@ -1,10 +1,34 @@
 from __future__ import annotations
 
+import logging
 from typing import Any
 
 import typer
 
+from .backends import FILE_SYSTEMS
+from .checksums import AVAILABLE_CHECKSUM_TYPES, Checksum
+from .logger import console_handler, logger
+
 app = typer.Typer()
+
+
+@app.callback()
+def logging_callback(
+    verbose: bool = typer.Option(False, "--verbose", "-v", help="Verbose output"),
+    debug: bool = typer.Option(False, "--debug", "-d", help="Debug output"),
+) -> Any:
+    # verbose is debug
+    trace = verbose and debug
+    verbose = debug or verbose
+    debug = verbose
+    log_level = logging.INFO
+
+    if debug:
+        log_level = logging.DEBUG
+    if trace:
+        log_level = logging.TRACE  # type: ignore[attr-defined]
+    logger.setLevel(log_level)
+    console_handler.setLevel(log_level)
 
 
 @app.command()
@@ -24,13 +48,32 @@ Smaller values will use less memory, larger sizes may have benefits in IO perfor
         default="/etc/xrootd/storage.xml",
         help="Path to the CMS storage catalog",
     ),
-    verbose: bool = False,
-    debug: bool = False,
+    file_system: str = typer.Option(
+        default="HDFS",
+        help="File system to use for reading the file",
+    ),
 ) -> None:
     """
     Get the checksum of a file.
     """
-    raise NotImplementedError()
+    # convert from MB to bytes
+    read_size *= 1024 * 1024
+    try:
+        hdfs = FILE_SYSTEMS[file_system](file_path, read_size)
+    except KeyError:
+        raise typer.Exit(
+            f"Unknown file system: {file_system}",
+            errno=1,
+        )
+    try:
+        checksum: Checksum = AVAILABLE_CHECKSUM_TYPES[checksum_type]()
+    except KeyError:
+        raise typer.Exit(
+            f"Unknown checksum type: {checksum_type}",
+            errno=1,
+        )
+    checksum = hdfs.get_checksum(checksum)
+    typer.echo(checksum.value)
 
 
 @app.command()
@@ -38,8 +81,6 @@ def verify(
     file_path: str,
     checksum_value: str,
     checksum_type: str = "adler32",
-    verbose: bool = False,
-    debug: bool = False,
 ) -> None:
     """
     Check if a file has the correct checksum.
