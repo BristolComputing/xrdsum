263f865e1e6f68717db7b3b99ff48470e9b250a1
kreczko <lkreczko@googlemail.com>
2023-03-08 16:27:47 +0000

    refactor: move int_to_hex and hex_to_int outside Checksum class

--- src/xrdsum/checksums/_base.py
+++ src/xrdsum/checksums/_base.py
@@ -1,7 +1,17 @@
 """Definition of the Checksum protocol."""
 from __future__ import annotations
 
+import struct
 from typing import Any, Iterable, Protocol
+
+
+def int_to_hex(value: int) -> str:
+    # return hex(value)[2:]
+    return "".join([f"{x:02x}" for x in struct.pack(">I", value)]).lower()
+
+
+def hex_to_int(value: str) -> int:
+    return int(value, 16)
 
 
 class Checksum(Protocol):
@@ -11,14 +21,6 @@ class Checksum(Protocol):
     value: str = "N/A"
     bytes_read: int = 0
     number_of_buffers_read: int = 0
-
-    def int_to_hex(self, value: int) -> str:
-        """Converts integer to hex representation"""
-        raise NotImplementedError()
-
-    def hex_to_int(self, value: str) -> int:
-        """Converts hex representation to integer"""
-        raise NotImplementedError()
 
     def calculate(self, file_buffer: Iterable[Any]) -> str:
         """Calculates the checksum"""
--- src/xrdsum/checksums/adler32.py
+++ src/xrdsum/checksums/adler32.py
@@ -2,12 +2,11 @@
 from __future__ import annotations
 
 import logging
-import struct
 import zlib
 from typing import Any, Iterable
 
 from ..logger import APP_LOGGER_NAME
-from ._base import Checksum
+from ._base import Checksum, int_to_hex
 
 log = logging.getLogger(APP_LOGGER_NAME)
 
@@ -17,13 +16,6 @@ class Adler32(Checksum):
     from https://github.com/snafus/cephsum/blob/master/cephsum/adler32.py"""
 
     name: str = "adler32"
-
-    def int_to_hex(self, value: int) -> str:
-        # return hex(value)[2:]
-        return "".join([f"{x:02x}" for x in struct.pack(">I", value)]).lower()
-
-    def hex_to_int(self, value: str) -> int:
-        return int(value, 16)
 
     def calculate(self, file_buffer: Iterable[Any]) -> str:
         value = 1
@@ -36,12 +28,12 @@ class Adler32(Checksum):
             log.trace(  # type: ignore[attr-defined]
                 "%s: %s %s %s",
                 self.name,
-                self.int_to_hex(value),
+                int_to_hex(value),
                 len(buffer),
                 bytes_read,
             )
 
-        self.value = self.int_to_hex(value)
+        self.value = int_to_hex(value)
         self.bytes_read = bytes_read
         self.number_of_buffers_read = number_of_buffers_read
 
