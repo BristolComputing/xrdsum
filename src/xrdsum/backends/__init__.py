db1af307065c1822095e90db8f7f91b7c19528eb
kreczko <lkreczko@googlemail.com>
2023-12-06 12:51:13 +0000

    feat: add HTTPBackend

--- src/xrdsum/backends/__init__.py
+++ src/xrdsum/backends/__init__.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 from ._base import XrdsumBackend
 from ._cephfs import CephFSBackend
 from ._hdfs import HDFSBackend
+from ._http import HttpBackend
 
 FILE_SYSTEMS: dict[str, type[XrdsumBackend]] = {
     "HDFS": HDFSBackend,
     "CephFS": CephFSBackend,
+    "HTTP": HttpBackend,
 }
 
 __all__ = ["FILE_SYSTEMS"]
