272995c35a50606e8ee735162ca4a98527b5866f
kreczko <lkreczko@googlemail.com>
2024-06-11 16:28:24 +0100

    feat: add POSIX backend

--- src/xrdsum/backends/__init__.py
+++ src/xrdsum/backends/__init__.py
@@ -5,11 +5,13 @@ from ._base import XrdsumBackend
 from ._cephfs import CephFSBackend
 from ._hdfs import HDFSBackend
 from ._http import HttpBackend
+from ._posix import POSIXBackend
 
 FILE_SYSTEMS: dict[str, type[XrdsumBackend]] = {
     "HDFS": HDFSBackend,
     "CephFS": CephFSBackend,
     "HTTP": HttpBackend,
+    "POSIX": POSIXBackend,
 }
 
 __all__ = ["FILE_SYSTEMS"]
